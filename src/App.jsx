b04fe7ac19939c2b36affcaf5551d0aa1e62085c
Gaurav Thakur <shinra420070186@gmail.com>
2026-04-02 23:13:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import React from 'react';
-import GameBoard from './GameBoard';
-import Galaxy from './Galaxy';
-import './index.css';
+import GameBoard from './GameBoard'
 
-export default function App() {
+function App() {
   return (
-    <div className="relative w-full h-screen overflow-hidden bg-black">
-      
-      {/* 1. The Galaxy Background (Sits at the very back) */}
-      <div className="absolute inset-0 z-0">
-        <Galaxy />
-      </div>
-
-      {/* 2. Your Game UI Wrapper */}
-      {/* CRITICAL: pointer-events-none here lets your finger pass through the empty screen space */}
-      <div className="absolute inset-0 z-10 pointer-events-none flex flex-col items-center justify-center">
-        
-        {/* This loads your actual Mafia game */}
-        <GameBoard />
-
-      </div>
-    </div>
-  );
+    <GameBoard />
+  )
 }
+
+export default App
