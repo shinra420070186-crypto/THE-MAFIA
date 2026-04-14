5be9085f157e7159f5e8845d646dcbf2747efbf7
Gaurav Thakur <shinra420070186@gmail.com>
2026-04-14 12:44:37 +0530

    Update sfx.js

--- src/sfx.js
+++ src/sfx.js
@@ -1,4 +1,28 @@
-export const sfx = {
-  init: () => {},
-  tap: () => {}
-};
+class AudioManager {
+  constructor() {
+    this.nightBgm = new Audio('/night-bgm.mp3');
+    this.nightBgm.loop = true;
+  }
+
+  playNightBgm() {
+    // Only play if it's currently paused to avoid restarting it if already playing
+    if (this.nightBgm.paused) {
+      this.nightBgm.play().catch(e => console.log("Auto-play prevented by browser:", e));
+    }
+  }
+
+  stopNightBgm() {
+    this.nightBgm.pause();
+    this.nightBgm.currentTime = 0;
+  }
+
+  init() {
+    // Placeholder for future global init
+  }
+
+  tap() {
+    // Placeholder for future UI tap sound
+  }
+}
+
+export const sfx = new AudioManager();
